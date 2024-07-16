2e630d7975df8d54b5f27dc45809b18ce6a1a1ea
HippolyteWeber <hippolyte.weber@gmail.com>
2024-07-16 12:09:27 +0200

    database init first part

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,34 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+
+-- SQLBook: Code
+CREATE TABLE role (
+    role_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    role VARCHAR(80) 
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+
+CREATE TABLE user (
+    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pseudo VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INT UNSIGNED NULL,
+    FOREIGN KEY (role_id) REFERENCES role (role_id)
 );
+
+
+INSERT INTO role (role) VALUES 
+('Admin'),
+('User'),
+('Manager'),
+('Guest'),
+('Operator');
+
+CREATE TRIGGER before_user_insert
+BEFORE INSERT ON User
+FOR EACH ROW
+BEGIN
+    IF NEW.role_id IS NULL THEN
+        SET NEW.role_id = (SELECT role_id FROM Role WHERE role = 'User');
+    END IF;
+END 
+
